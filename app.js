4cd16ce09fd4e00cc1c5e22b8daaa0d0756e8be0
Erica Thompson <younique98@gmail.com>
2021-05-21 07:56:57 -0700

    dev share is now working and the user can be authenticated with Google oauth

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
-app.use('/stories', require('./routes/stories'))
+app.use('/tips', require('./routes/tips'))
 
 const PORT = process.env.PORT || 3000
 
