74649eaec3e5d7cad5512b4bf4d3f66e5758da31
Erica Thompson <younique98@gmail.com>
2021-06-06 12:16:36 -0700

    fixed the dashboard tips as they were not showing up upon creation

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,32 @@
-const express = require('express')
-const router = express.Router()
-const { ensureAuth, ensureGuest } = require('../middleware/auth')
+const express = require("express");
+const router = express.Router();
+//utilizing object destructoring
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
-const Tip = require('../models/DevTip')
+const Tip = require("../models/Tip");
 
 // @desc    Login/Landing page
 // @route   GET /
-router.get('/', ensureGuest, (req, res) => {
-  res.render('login', {
-    layout: 'login',
-  })
-})
+router.get("/", ensureGuest, (req, res) => {
+  res.render("login", {
+    layout: "login",
+  });
+});
 
 // @desc    Dashboard
-// @route   GET /dashboard
-router.get('/dashboard', ensureAuth, async (req, res) => {
+// @route   GET /dashboard & Utilizes the TipSchema in models
+// req = request then lean() allows data to pass in data using handlebar template, render it and read that data
+router.get("/dashboard", ensureAuth, async (req, res) => {
   try {
-    const devTips = await Tip.find({ user: req.user.id }).lean()
-    res.render('dashboard', {
+    const tips = await Tip.find({ user: req.user.id }).lean();
+    res.render("dashboard", {
       name: req.user.firstName,
-      devTips,
-    })
+      tips,
+    });
   } catch (err) {
-    console.error(err)
-    res.render('error/500')
+    console.error(err);
+    res.render("error/500");
   }
-})
+});
 
-module.exports = router
+module.exports = router;
--- routes/tips.js
+++ routes/tips.js
@@ -1,160 +1,159 @@
-const express = require('express')
-const router = express.Router()
-const { ensureAuth } = require('../middleware/auth')
+const express = require("express");
+const router = express.Router();
+const { ensureAuth } = require("../middleware/auth");
 
-const DevTip = require('../models/DevTip')
-
+const Tip = require("../models/Tip");
 // @desc    Show add page
 // @route   GET /tips/add
-router.get('/add', ensureAuth, (req, res) => {
-  res.render('tips/add')
-})
+router.get("/add", ensureAuth, (req, res) => {
+  res.render("tips/add");
+});
 
 // @desc    Process add form
 // @route   POST /tips
-router.post('/', ensureAuth, async (req, res) => {
+router.post("/", ensureAuth, async (req, res) => {
   try {
-    req.body.user = req.user.id
-    await DevTip.create(req.body)
-    res.redirect('/dashboard')
+    req.body.user = req.user.id;
+    await Tip.create(req.body);
+    res.redirect("/dashboard");
   } catch (err) {
-    console.error(err)
-    res.render('error/500')
+    console.error(err);
+    res.render("error/500");
   }
-})
+});
 
 // @desc    Show all tips
 // @route   GET /tips
-router.get('/', ensureAuth, async (req, res) => {
+router.get("/", ensureAuth, async (req, res) => {
   try {
-    const tips = await DevTip.find({ status: 'public' })
-      .populate('user')
-      .sort({ createdAt: 'desc' })
-      .lean()
+    const tips = await Tip.find({ status: "public" })
+      .populate("user")
+      .sort({ createdAt: "desc" })
+      .lean();
 
-    res.render('tips/index', {
+    res.render("tips/index", {
       tips,
-    })
+    });
   } catch (err) {
-    console.error(err)
-    res.render('error/500')
+    console.error(err);
+    res.render("error/500");
   }
-})
+});
 
-// @desc    Show single DevTip
+// @desc    Show single Tip
 // @route   GET /tips/:id
-router.get('/:id', ensureAuth, async (req, res) => {
+router.get("/:id", ensureAuth, async (req, res) => {
   try {
-    let tip = await DevTip.findById(req.params.id).populate('user').lean()
+    let tip = await Tip.findById(req.params.id).populate("user").lean();
 
     if (!tip) {
-      return res.render('error/404')
+      return res.render("error/404");
     }
 
-    if (tip.user._id != req.user.id && tip.status == 'private') {
-      res.render('error/404')
+    if (tip.user._id != req.user.id && tip.status == "private") {
+      res.render("error/404");
     } else {
-      res.render('tips/show', {
+      res.render("tips/show", {
         tip,
-      })
+      });
     }
   } catch (err) {
-    console.error(err)
-    res.render('error/404')
+    console.error(err);
+    res.render("error/404");
   }
-})
+});
 
 // @desc    Show edit page
 // @route   GET /tips/edit/:id
-router.get('/edit/:id', ensureAuth, async (req, res) => {
+router.get("/edit/:id", ensureAuth, async (req, res) => {
   try {
-    const tip = await DevTip.findOne({
+    const tip = await Tip.findOne({
       _id: req.params.id,
-    }).lean()
+    }).lean();
 
     if (!tip) {
-      return res.render('error/404')
+      return res.render("error/404");
     }
 
     if (tip.user != req.user.id) {
-      res.redirect('/tips')
+      res.redirect("/tips");
     } else {
-      res.render('tips/edit', {
+      res.render("tips/edit", {
         tip,
-      })
+      });
     }
   } catch (err) {
-    console.error(err)
-    return res.render('error/500')
+    console.error(err);
+    return res.render("error/500");
   }
-})
+});
 
 // @desc    Update tip
 // @route   PUT /tips/:id
-router.put('/:id', ensureAuth, async (req, res) => {
+router.put("/:id", ensureAuth, async (req, res) => {
   try {
-    let tip = await DevTip.findById(req.params.id).lean()
+    let tip = await Tip.findById(req.params.id).lean();
 
     if (!tip) {
-      return res.render('error/404')
+      return res.render("error/404");
     }
 
     if (tip.user != req.user.id) {
-      res.redirect('/tips')
+      res.redirect("/tips");
     } else {
-      tip = await tip.findOneAndUpdate({ _id: req.params.id }, req.body, {
+      tip = await Tip.findOneAndUpdate({ _id: req.params.id }, req.body, {
         new: true,
         runValidators: true,
-      })
+      });
 
-      res.redirect('/dashboard')
+      res.redirect("/dashboard");
     }
   } catch (err) {
-    console.error(err)
-    return res.render('error/500')
+    console.error(err);
+    return res.render("error/500");
   }
-})
+});
 
 // @desc    Delete tip
 // @route   DELETE /tips/:id
-router.delete('/:id', ensureAuth, async (req, res) => {
+router.delete("/:id", ensureAuth, async (req, res) => {
   try {
-    let tip = await DevTip.findById(req.params.id).lean()
+    let tip = await Tip.findById(req.params.id).lean();
 
     if (!tip) {
-      return res.render('error/404')
+      return res.render("error/404");
     }
 
     if (tip.user != req.user.id) {
-      res.redirect('/tips')
+      res.redirect("/tips");
     } else {
-      await tip.remove({ _id: req.params.id })
-      res.redirect('/dashboard')
+      await Tip.remove({ _id: req.params.id });
+      res.redirect("/dashboard");
     }
   } catch (err) {
-    console.error(err)
-    return res.render('error/500')
+    console.error(err);
+    return res.render("error/500");
   }
-})
+});
 
 // @desc    User tips
 // @route   GET /tips/user/:userId
-router.get('/user/:userId', ensureAuth, async (req, res) => {
+router.get("/user/:userId", ensureAuth, async (req, res) => {
   try {
-    const tips = await DevTip.find({
+    const tips = await Tip.find({
       user: req.params.userId,
-      status: 'public',
+      status: "public",
     })
-      .populate('user')
-      .lean()
+      .populate("user")
+      .lean();
 
-    res.render('tips/index', {
+    res.render("tips/index", {
       tips,
-    })
+    });
   } catch (err) {
-    console.error(err)
-    res.render('error/500')
+    console.error(err);
+    res.render("error/500");
   }
-})
+});
 
-module.exports = router
+module.exports = router;
